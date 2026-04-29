bac130e8431ea10af1a419b8b6660a8632ad0400
Rida Farhan <42339599+Ridafarhan99@users.noreply.github.com>
2026-04-29 19:56:22 +0530

    feat: list instruction implemented

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ readJSON();
 // fs.writeFileSync("task.json", JSON.stringify(data, null, 4), "utf8");
 
 const instruction = process.argv[2];
+const status = process.argv[3];
 // console.log(instruction);
 
 const saveJSON = (data) => {
@@ -37,7 +38,7 @@ if (instruction == "add") {
   let tasks = readJSON();
   let myID = tasks.length == 0 ? 1 : tasks[tasks.length - 1].id + 1;
   let desc = "adding data into json";
-  let status = null;
+  let status = "todo";
   let createdAt = new Date();
   let updatedAt = new Date();
   let tempValue = {
@@ -70,5 +71,37 @@ if (instruction == "update") {
     updatedTask.data = updatedValue;
     updatedTask.updatedAt = new Date();
     saveJSON(tasks);
+  }
+}
+
+if (instruction == "list") {
+  let tasks = readJSON();
+  if (status == undefined) {
+    let num = 1;
+    tasks.forEach((task) => {
+      console.log(`${num} Task Name: ${task.data}, Status: ${task.status}`);
+      num++;
+    });
+  } else if (status == "todo") {
+    let todoTasks = tasks.filter((task) => task.status == "todo");
+    let num = 1;
+    todoTasks.forEach((task) => {
+      console.log(`${num} Task Name: ${task.data}, Status: ${task.status}`);
+      num++;
+    });
+  } else if (status == "done") {
+    let todoTasks = tasks.filter((task) => task.status == "done");
+    let num = 1;
+    todoTasks.forEach((task) => {
+      console.log(`${num} Task Name: ${task.data}, Status: ${task.status}`);
+      num++;
+    });
+  } else if (status == "in-progress") {
+    let todoTasks = tasks.filter((task) => task.status == "in-progress");
+    let num = 1;
+    todoTasks.forEach((task) => {
+      console.log(`${num} Task Name: ${task.data}, Status: ${task.status}`);
+      num++;
+    });
   }
 }
